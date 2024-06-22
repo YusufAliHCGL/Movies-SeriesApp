901a292e75f69f4d61f5ab3f13e1cd5ed3f4c235
Yusuf Ali <hyus3158@gmail.com>
2024-06-22 18:11:08 +0300

    User experience improved. Loading and Error cases were handled.

--- app/src/main/java/com/example/moviesseriesapp/presentation/movies/views/MoviesListRow.kt
+++ app/src/main/java/com/example/moviesseriesapp/presentation/movies/views/MoviesListRow.kt
@@ -1,6 +1,7 @@
 package com.example.moviesseriesapp.presentation.movies.views
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -11,8 +12,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -42,15 +46,50 @@ fun MoviesListRow(movie: Movie, onClick: (String) -> Unit) {
 
 
 
-        Row( modifier = Modifier.fillMaxWidth().padding(4.dp).clickable {
-            onClick(movie.imdbID)
-        }) {
-            Image(painter = rememberAsyncImagePainter(model = movie.poster), contentDescription = movie.title, modifier = Modifier.size(220.dp, 220.dp).clip(
-                RoundedCornerShape(40.dp)
-            ).border(width = 2.dp, brush = gradient, shape = RoundedCornerShape(40.dp)), contentScale = ContentScale.Crop)
+        Row( modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp)
+            .clickable {
+                onClick(movie.imdbID)
+            }) {
+            val isLoading = remember {
+                mutableStateOf(false)
+            }
+            val isError = remember {
+                mutableStateOf(false)
+            }
+            Box(modifier = Modifier
+                .size(220.dp, 220.dp)
+                .clip(RoundedCornerShape(40.dp))
+                .background(Color.White)
+                .border(width = 2.dp, brush = gradient, shape = RoundedCornerShape(40.dp))) {
+                Image(painter = rememberAsyncImagePainter(model = movie.poster, onLoading = {isLoading.value = true
+                                                                                            isError.value = false},
+                    onSuccess = {isLoading.value = false
+                                isError.value = false},
+                    onError = {
+                        isError.value = true
+                        isLoading.value = false
+                    }),
+                    contentDescription = movie.title,
+                    modifier = Modifier
+                        .size(220.dp, 220.dp)
+                        .clip(
+                            RoundedCornerShape(40.dp)
+                        )
+                        .border(width = 2.dp, brush = gradient, shape = RoundedCornerShape(40.dp)), contentScale = ContentScale.Crop)
+                if (isLoading.value) {
+                    CircularProgressIndicator(color = Color.Yellow, modifier = Modifier.align(
+                        Alignment.Center))
+                }
+                if (isError.value) {
+                    Text(text = "Failed to load image!", color = Color.Red, modifier = Modifier.align(Alignment.Center))
+                }
+            }
+
             Column(modifier = Modifier.align(Alignment.CenterVertically)) {
-                Text(text = movie.title, textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth(), overflow = TextOverflow.Ellipsis, fontSize = 32.sp, lineHeight = 32.sp, fontWeight = FontWeight.W600)
-                Text(text = movie.year, textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth(), overflow = TextOverflow.Ellipsis, fontSize = 28.sp, fontWeight = FontWeight.W500)
+                Text(text = movie.title, textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth(), overflow = TextOverflow.Ellipsis, fontSize = 32.sp, lineHeight = 32.sp, fontWeight = FontWeight.W600, maxLines = 4)
+                Text(text = movie.year, textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth(), overflow = TextOverflow.Ellipsis, fontSize = 28.sp, fontWeight = FontWeight.W500, maxLines = 1)
             }
         }
     }
